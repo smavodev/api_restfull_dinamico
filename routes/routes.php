86aa781fae2f56cb9ffcda77f35a6dfd609a4903
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-19 12:06:41 -0500

    Configuración del archivo .htaccess, configuracion de las rutas para consultar una base de datos

--- routes/routes.php
+++ routes/routes.php
@@ -1,12 +1,19 @@
 <?php
 
-$json = array(
 
-    'status' => 200,
-    'results' => 'Success'
+$routesArray = explode("/", $_SERVER['REQUEST_URI']); /* obtener el dato que se envia en el indice  */
+$routesArray = array_filter($routesArray); /* Filtro para obtener un indice*/
 
-);
+/*===== Cuando no se hace ninguna petición a la API =====*/
+if (count($routesArray) == 0) {
+    $json = array(
+        'status' => 404,
+		'results' => 'Not Found'
+    );
 
-echo json_encode($json);
+	echo json_encode($json, http_response_code($json["status"]));
 
-return;
+    return;
+}
+
+/*===== Cuando si se hace una petición a la API =====*/
