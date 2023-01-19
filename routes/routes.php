9caf219199fd61d4a6bc5ef13f53903dfd2e11de
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-19 12:26:26 -0500

    Habilitar servicios HTTP: GET, POST, PUT, DELETE

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,7 @@
 
 
 $routesArray = explode("/", $_SERVER['REQUEST_URI']); /* obtener el dato que se envia en el indice  */
-$routesArray = array_filter($routesArray); /* Filtro para obtener un indice*/
+$routesArray = array_filter($routesArray); /* Filtro para obtener el indice*/
 
 /*===== Cuando no se hace ninguna petición a la API =====*/
 if (count($routesArray) == 0) {
@@ -16,3 +16,52 @@ if (count($routesArray) == 0) {
     return;
 }
 
+
+/*===== Cuando Si se hace ninguna petición a la API =====*/
+if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
+
+    /*===== Peticiones GET =====*/
+	if($_SERVER['REQUEST_METHOD'] == "GET"){
+        
+        $json = array(
+            'status' => 200,
+            'results' => 'Solicitud GET'
+        );
+
+        echo json_encode($json, http_response_code($json["status"]));
+	}
+
+	/*===== Peticiones POST =====*/
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+        $json = array(
+            'status' => 200,
+            'results' => 'Solicitud POST'
+        );
+
+        echo json_encode($json, http_response_code($json["status"]));
+	}
+
+	/*===== Peticiones PUT =====*/
+	if($_SERVER['REQUEST_METHOD'] == "PUT"){
+
+        $json = array(
+            'status' => 200,
+            'results' => 'Solicitud PUT'
+        );
+
+		echo json_encode($json, http_response_code($json["status"]));
+	}
+
+	/*===== Peticiones DELETE =====*/
+	if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+
+        $json = array(
+            'status' => 200,
+            'results' => 'Solicitud DELETE'
+        );
+
+		echo json_encode($json, http_response_code($json["status"]));
+	}
+
+}
