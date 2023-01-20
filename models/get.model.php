09a3112dbb2da52c21b766ad1b7c66b430c1f575
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-19 21:24:06 -0500

    Peticion GET + Filtro con varios valores

--- models/get.model.php
+++ models/get.model.php
@@ -17,11 +17,33 @@ class GetModel {
 
     /*===== Peticiones GET con filtro =====*/
     static public function getDataFilter($table, $select, $linkTo, $equalTo){
-        $sql = "SELECT $select FROM $table WHERE $linkTo = :$linkTo  ";
+        
+		$linkToArray = explode(",",$linkTo);
+		$equalToArray = explode(",",$equalTo);
+		$linkToText = "";
+
+		if(count($linkToArray)>1){
+
+			foreach ($linkToArray as $key => $value) {
+				if($key > 0){
+					$linkToText .= "AND ".$value." = :".$value." ";
+				}
+			}
+
+		}
+        
+        $sql = "SELECT $select FROM $table WHERE $linkToArray[0] = :$linkToArray[0] $linkToText ";
+        
         $stmt = Connection::connect()->prepare($sql);
-        $stmt -> bindParam(":".$linkTo, $equalTo, PDO::PARAM_STR);
+
+        foreach ($linkToArray as $key => $value) {
+			$stmt -> bindParam(":".$value, $equalToArray[$key], PDO::PARAM_STR);
+		}
+
         $stmt -> execute();
+
         return $stmt -> fetchAll(PDO::FETCH_CLASS);
+
     }
 
 }
