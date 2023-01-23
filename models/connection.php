225a3dae7fb75eb45b36befe9b350c7012caf51d
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-01-22 21:38:10 -0500

    Depuracion de Errores de tablas, columnas en Solicitudes GET,m ajuste de columnas globales al seleccionarlas

--- models/connection.php
+++ models/connection.php
@@ -27,4 +27,39 @@ class Connection{
 		return $link;
 	}
 
+
+	/*====== Validar existencia de una tabla en la bd =====*/
+	static public function getColumnsData($table, $columns){
+
+		/*====== Traer el nombre de la base de datos =====*/
+		$database = Connection::infoDatabase()["database"];
+
+		/*====== Traer todas las columnas de una tabla =====*/
+		$validate = Connection::connect()
+		->query("SELECT COLUMN_NAME AS item FROM information_schema.columns WHERE table_schema = '$database' AND table_name = '$table'")
+		->fetchAll(PDO::FETCH_OBJ);
+
+		/*====== Validamos existencia de la tabla =====*/
+		if(empty($validate)){
+			return null;
+		}else{
+
+			/*====== Ajuste de selección de columnas globales =====*/
+			if($columns[0] == "*"){
+				array_shift($columns);
+			}
+
+			/*====== Validamos existencia de columnas =====*/
+			$sum = 0;
+				
+			foreach ($validate as $key => $value) {
+				$sum += in_array($value->item, $columns);			
+			}
+
+			return $sum == count($columns) ? $validate : null;
+
+		}
+
+	}
+
 }
--- models/get.model.php
+++ models/get.model.php
@@ -7,6 +7,14 @@ class GetModel {
     /*===== Peticiones GET sin filtro =====*/
     static public function getData($table, $select, $orderBy, $orderMode, $startAt, $endAt){
 
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		$selectArray = explode(",",$select);
+		
+		if(empty(Connection::getColumnsData($table, $selectArray))){
+			return null;
+		}
+
+		
         /*===== Peticiones GET - Sin Ordenar Datos - Sin Limitar Datos =====*/
         $sql = "SELECT $select FROM $table ";
 
@@ -35,10 +43,25 @@ class GetModel {
 
     /*===== Peticiones GET con filtro =====*/
     static public function getDataFilter($table, $select, $linkTo, $equalTo, $orderBy, $orderMode, $startAt, $endAt){
+		
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		$linkToArray = explode(",",$linkTo);
+		$selectArray = explode(",",$select);
 
-        $linkToArray = explode(",", $linkTo);
-        $equalToArray = explode(",", $equalTo);
-        $linkToText = "";
+		foreach ($linkToArray  as $key => $value) {
+			array_push($selectArray, $value);
+		}
+
+		$selectArray = array_unique($selectArray);
+
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		if(empty(Connection::getColumnsData($table,$selectArray ))){	
+			return null;
+		}
+		
+		$equalToArray = explode(",",$equalTo);
+		$linkToText = "";
+
 
         if (count($linkToArray) > 1) {
 
@@ -91,6 +114,11 @@ class GetModel {
 
 			foreach ($relArray as $key => $value) {
 
+				/*====== Validar existencia de la tabla y de las columnas ======*/
+				if(empty(Connection::getColumnsData($value, ["*"]))){
+					return null;
+				}
+				
 				if($key > 0){
 					$innerJoinText .= "INNER JOIN ".$value." ON ".$relArray[0].".id_".$typeArray[$key]."_".$typeArray[0] ." = ".$value.".id_".$typeArray[$key]." ";
 				}
@@ -158,9 +186,15 @@ class GetModel {
 
 			foreach ($relArray as $key => $value) {
 
+				/*====== Validar existencia de la tabla ======*/
+				if(empty(Connection::getColumnsData($value, ["*"]))){
+					return null;
+				}
+
 				if($key > 0){
 					$innerJoinText .= "INNER JOIN ".$value." ON ".$relArray[0].".id_".$typeArray[$key]."_".$typeArray[0] ." = ".$value.".id_".$typeArray[$key]." ";
 				}
+
 			}
 
             /*===== Peticiones GET con filtro - Sin Ordenar Datos - Sin Limitar Datos =====*/
@@ -213,10 +247,25 @@ class GetModel {
 
     /*===== Peticiones GET para el buscador sin tablas relaciones =====*/
 	static public function getDataSearch($table, $select, $linkTo, $search,$orderBy,$orderMode,$startAt,$endAt){
-   
+   		
+		/*====== Validar existencia de la tabla y de las columnas ======*/
 		$linkToArray = explode(",",$linkTo);
+		$selectArray = explode(",",$select);
+
+		foreach ($linkToArray  as $key => $value) {
+			array_push($selectArray, $value);
+		}
+
+		$selectArray = array_unique($selectArray);
+		
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		if(empty(Connection::getColumnsData($table,$selectArray ))){
+			return null;
+		}
+
 		$searchArray = explode(",",$search);
-        $linkToText= "";
+		$linkToText = "";
+		
 		
         if(count($linkToArray)>1){
 
@@ -276,7 +325,6 @@ class GetModel {
     /*===== Peticiones GET para el buscador con tablas relaciones =====*/
 	static public function getRelDataSearch($rel, $type, $select, $linkTo, $search, $orderBy,$orderMode,$startAt,$endAt){
 
-
 		/*===== Organizamos los filtros =====*/
 		$linkToArray = explode(",",$linkTo);
 		$searchArray = explode(",",$search);
@@ -302,9 +350,13 @@ class GetModel {
 		if(count($relArray)>1){
 
 			foreach ($relArray as $key => $value) {
+				
+				/*====== Validar existencia de la tabla ======*/
+				if(empty(Connection::getColumnsData($value, ["*"]))){	
+					return null;
+				}
 
 				if($key > 0){
-
 					$innerJoinText .= "INNER JOIN ".$value." ON ".$relArray[0].".id_".$typeArray[$key]."_".$typeArray[0] ." = ".$value.".id_".$typeArray[$key]." ";
 				}
 			}
@@ -358,7 +410,34 @@ class GetModel {
 
     /*===== Peticiones GET para la seleccion de rangos sin tablas relaciones =====*/
 	static public function getDataRange($table,$select,$linkTo,$between1,$between2,$orderBy,$orderMode,$startAt,$endAt, $filterTo, $inTo){
-        
+		
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		$linkToArray = explode(",",$linkTo);
+
+		if($filterTo != null){
+			$filterToArray = explode(",",$filterTo);
+		}else{
+			$filterToArray =array();
+		}
+		
+		$selectArray = explode(",",$select);
+
+		foreach ($linkToArray  as $key => $value) {
+			array_push($selectArray, $value);
+		}
+
+		foreach ($filterToArray  as $key => $value) {
+			array_push($selectArray, $value);
+		}
+
+		$selectArray = array_unique($selectArray);
+
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		if(empty(Connection::getColumnsData($table, $selectArray))){	
+			return null;
+		}
+
+		
 		$filter = "";
 
 		if($filterTo != null && $inTo != null){
@@ -401,6 +480,15 @@ class GetModel {
 
 	/*===== Peticiones GET para la seleccion de rangos con tablas relaciones =====*/
 	static public function getRelDataRange($rel,$type,$select,$linkTo,$between1,$between2,$orderBy,$orderMode,$startAt,$endAt, $filterTo, $inTo){
+		
+		/*====== Validar existencia de la tabla y de las columnas ======*/
+		$linkToArray = explode(",",$linkTo);
+		
+		if($filterTo != null){
+			$filterToArray = explode(",",$filterTo);
+		}else{
+			$filterToArray =array();
+		}
 
 		$filter = "";
 
@@ -415,7 +503,12 @@ class GetModel {
 		if(count($relArray)>1){
 
 			foreach ($relArray as $key => $value) {
-	
+			
+				/*====== Validar existencia de la tabla ======*/
+				if(empty(Connection::getColumnsData($value, ["*"]))){
+					return null;
+				}
+
 				if($key > 0){
 					$innerJoinText .= "INNER JOIN ".$value." ON ".$relArray[0].".id_".$typeArray[$key]."_".$typeArray[0]." = ".$value.".id_".$typeArray[$key]." ";
 				}
