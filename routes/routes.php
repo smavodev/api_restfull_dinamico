f3e95cd43808f739d7ada729befb12a0b0992c7e
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-02-01 17:35:50 -0500

    Metodo PUT, update Get

--- routes/routes.php
+++ routes/routes.php
@@ -26,26 +26,17 @@ if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
 
     /*===== Peticiones GET =====*/
 	if($_SERVER['REQUEST_METHOD'] == "GET"){
-        
         include "services/get.php";
-
 	}
 
 	/*===== Peticiones POST =====*/
 	if($_SERVER['REQUEST_METHOD'] == "POST"){
-
         include "services/post.php";
 	}
 
 	/*===== Peticiones PUT =====*/
 	if($_SERVER['REQUEST_METHOD'] == "PUT"){
-
-        $json = array(
-            'status' => 200,
-            'results' => 'Solicitud PUT'
-        );
-
-		echo json_encode($json, http_response_code($json["status"]));
+        include "services/put.php";
 	}
 
 	/*===== Peticiones DELETE =====*/
