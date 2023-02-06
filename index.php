0fdc5ef9d1ebb642f9be03265eab78999f5af433
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-02-05 20:23:53 -0500

    Add CORS

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ ini_set("log_errors", 1);
 ini_set("error_log",  "C:/xampp/htdocs/apirest-dinamica-full/php_error_log");
 
 
+
+/*======= CORS ======*/
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
+
+
 /*===== Requerimientos =====*/
 /* require_once "models/conection.php";
 echo '<pre>'; print_r(Connection::infoDatabase()); echo '</pre>';
